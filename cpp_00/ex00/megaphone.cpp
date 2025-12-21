998869c7dbc6ea8aa0acae4eb848c98862eb3a41
izhans <izhansastre@gmail.com>
2025-12-21 19:48:38 +0100

    cpp_00 ex00

---
+++ cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <cstring>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			std::string arg = std::string(argv[i]);
+	
+			for (size_t j = 0; j < arg.length(); j++)
+				std::cout << (char) toupper(arg[j]);
+		}
+	}
+	std::cout << std::endl;
+
+	return 0;
+}
