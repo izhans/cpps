d910f94f5035dc95e96dd85cd83d16d7e4a650ec
izhans <izhansastre@gmail.com>
2026-04-29 14:02:28 +0200

    cpp01 ex02

---
+++ cpp_01/ex02/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	std::string s = "HI THIS IS BRAIN";
+	std::string *stringPTR = &s; // is a pointer (same as c)
+	std::string &stringREF = s; // is an alias (different name, same memory direction)
+
+	std::cout << "Memory adresses" << std::endl;
+	std::cout << "s: " << &s << std::endl;
+	std::cout << "stringPTR: " << &stringPTR << std::endl;
+	std::cout << "stringREF: " << &stringREF << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Values" << std::endl;
+	std::cout << "s: " << s << std::endl;
+	std::cout << "stringPTR: " << *stringPTR << std::endl;
+	std::cout << "stringREF: " << stringREF << std::endl;
+
+	return 0;
+}
