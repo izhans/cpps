9a0a169ab43b205e10a502d120696534e69019d5
izhans <izhansastre@gmail.com>
2026-04-29 13:47:49 +0200

    cpp01 ex00

---
+++ cpp_01/ex00/main.cpp
@@ -0,0 +1,16 @@
+#include "Zombie.hpp"
+
+int main()
+{
+	std::cout << "newZombie Alfred" << std::endl;
+	Zombie *z = newZombie("Alfred");
+	z->announce();
+	std::cout << std::endl;
+
+	std::cout << "randomChump Lauren" << std::endl;
+	randomChump("Lauren");
+	std::cout << std::endl;
+
+	delete z;
+	return 0;
+}
